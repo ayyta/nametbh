5366965a547aebe28e21870510cece16133fb483
Eric19La <eric.la7722@gmail.com>
2024-07-21 00:46:46 -0700

    Imported Home Logo needs resizing

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -1,10 +1,23 @@
 import Link from "next/link"
+import Image from "next/image";
 
 export default function Navbar() {
+
+  const imageStyle = {
+    borderRadius: '50',
+    border: '1px solid #fff',
+  }
 
   return (
     <nav className="sidebar">
       <div className="sidebar-link">
+        <Image 
+          src="/Home.svg"
+          width={200}
+          height={200}
+          style={imageStyle}
+          alt="Home logo"
+        />
         <Link href="/">
           Home
         </Link>
