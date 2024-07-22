76b2b5d02936bd707cb9570c2516c35c23a6ae06
Eric19La <eric.la7722@gmail.com>
2024-07-21 23:24:18 -0700

    Navbar icons loaded

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -5,22 +5,67 @@ export default function Navbar() {
 
   return (
     <nav className="sidebar">
-      <div className="sidebar-link">
-        <Image 
-          src="/Home.svg"
-          width={44}
-          height={0}
-          alt="Home logo"
-          className="sidebar-link-image"
-        />
-        <Link href="/">
-          Home
-        </Link>
-      </div>
-      <div className="sidebar-link">
-        <Link href="/following">
-          Following
-        </Link>
+      <div className="sidebar-container">
+        <div className="sidebar-link">
+          <Link href="/" className="sidebar-link-title">
+            <Image 
+              src="/Home.svg"
+              width={44}
+              height={0}
+              alt="Home logo"
+              className="sidebar-link-image"
+            />
+            Home
+          </Link>
+        </div>
+        <div className="sidebar-link">
+          <Link href="/following" className="sidebar-link-title">
+            <Image
+              src="/favorite.svg"
+              width={44}
+              height={0}
+              alt="Following logo"
+              className="sidebar-link-image"
+            />
+            Following
+          </Link>
+        </div>
+        <div className="sidebar-link">
+          <Link href="/friends" className="sidebar-link-title">
+            <Image
+              src="/Friends Icon.svg"
+              width={44}
+              height={0}
+              alt="Friends logo"
+              className="sidebar-link-image"
+            />
+            Friends
+          </Link>
+        </div>
+        <div className="sidebar-link">
+          <Link href="/inbox" className="sidebar-link-title">
+            <Image
+              src="/Inbox.svg"
+              width={44}
+              height={0}
+              alt="Inbox logo"
+              className="sidebar-link-image"
+            />
+            Inbox
+          </Link>
+        </div>
+        <div className="sidebar-link">
+          <Link href="/" className="sidebar-link-title">
+            <Image 
+              src="/Home.svg"
+              width={44}
+              height={0}
+              alt="Home logo"
+              className="sidebar-link-image"
+            />
+            Home
+          </Link>
+        </div>
       </div>
     </nav>
   );
