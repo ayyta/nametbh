5a2e999002b6528c4ade74da906124f4f1bd2ef7
Eric19La <eric.la7722@gmail.com>
2024-07-22 01:14:48 -0700

    Need to fix up Avatar Section

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -6,65 +6,84 @@ export default function Navbar() {
   return (
     <nav className="sidebar">
       <div className="sidebar-container">
-        <div className="sidebar-link">
-          <Link href="/" className="sidebar-link-title">
-            <Image 
-              src="/Home.svg"
-              width={44}
-              height={0}
-              alt="Home logo"
-              className="sidebar-link-image"
-            />
-            Home
-          </Link>
+        <div className="sidebar-upper-section">
+          <div className="sidebar-link">
+            <Link href="/" className="sidebar-link-title">
+              <Image 
+                src="/Home.svg"
+                width={44}
+                height={0}
+                alt="Home logo"
+                className="sidebar-link-image"
+              />
+              Home
+            </Link>
+          </div>
+          <div className="sidebar-link">
+            <Link href="/following" className="sidebar-link-title">
+              <Image
+                src="/favorite.svg"
+                width={44}
+                height={0}
+                alt="Following logo"
+                className="sidebar-link-image"
+              />
+              Following
+            </Link>
+          </div>
+          <div className="sidebar-link">
+            <Link href="/friends" className="sidebar-link-title">
+              <Image
+                src="/Friends Icon.svg"
+                width={44}
+                height={0}
+                alt="Friends logo"
+                className="sidebar-link-image"
+              />
+              Friends
+            </Link>
+          </div>
+          <div className="sidebar-link">
+            <Link href="/inbox" className="sidebar-link-title">
+              <Image
+                src="/Inbox.svg"
+                width={44}
+                height={0}
+                alt="Inbox logo"
+                className="sidebar-link-image"
+              />
+              Inbox
+            </Link>
+          </div>
         </div>
-        <div className="sidebar-link">
-          <Link href="/following" className="sidebar-link-title">
+        <div className="sidebar-lower-section">
+          <div className="sidebar-link">
+            <Link href="/" className="sidebar-link-title">
+              <Image 
+                src="/Plus square.svg"
+                width={44}
+                height={0}
+                alt="Plus logo"
+                className="sidebar-link-image"
+              />
+              Upload
+            </Link>
+          </div>
+          <div className="sidebar-link avatar-container">
             <Image
-              src="/favorite.svg"
-              width={44}
+              src="/Generic avatar.svg"
+              width={62}
               height={0}
-              alt="Following logo"
-              className="sidebar-link-image"
+              alt="Avatar logo"
+              className="avatar-logo"
             />
-            Following
-          </Link>
-        </div>
-        <div className="sidebar-link">
-          <Link href="/friends" className="sidebar-link-title">
-            <Image
-              src="/Friends Icon.svg"
-              width={44}
-              height={0}
-              alt="Friends logo"
-              className="sidebar-link-image"
-            />
-            Friends
-          </Link>
-        </div>
-        <div className="sidebar-link">
-          <Link href="/inbox" className="sidebar-link-title">
-            <Image
-              src="/Inbox.svg"
-              width={44}
-              height={0}
-              alt="Inbox logo"
-              className="sidebar-link-image"
-            />
-            Inbox
-          </Link>
-        </div>
-        <div className="sidebar-link">
-          <Link href="/" className="sidebar-link-title">
-            <Image 
-              src="/Home.svg"
-              width={44}
-              height={0}
-              alt="Home logo"
-              className="sidebar-link-image"
-            />
-            Home
-          </Link>
+            <div className="avatar-name">
+              huge burger
+              {/* <div>
+                @hugeburger
+              </div> */}
+            </div>
+          </div>
         </div>
       </div>
     </nav>
