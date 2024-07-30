8925921e35ac861995d0c52b3ee890bfa6950daf
Eric19La <Eric19La@github.com>
2024-07-30 13:47:15 -0700

    Change

--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -5,18 +5,80 @@ import { useState } from "react";
 
 export default function Navbar() {
 
-  const [isBold, setIsBold] = useState(0);
+  // const [isBold, setIsBold] = useState(0);
+  const [selectedItem, setSelectedItem] = useState('Home')
 
-  function handleClick() {
-    console.log("hi");
+
+  function handleClick(item) {
+    // 
+    const lastItem = selectedItem;
+    setSelectedItem(item);
+
+    // unbold lastItem
+    // apply bold to new item
+  }
+
+  let navbarItems = [
+    {
+      'name': "Home", 
+      "link": "/"
+    },
+    {
+      "name": "Following",
+      "link": "/following"
+    },
+    {
+      "name": "Friends",
+      "link": "/friends"
+    },
+    {
+      "name": "Inbox",
+      "link": "/inbox"
+    },
+    {
+      "name": "Upload",
+      "link": "/upload"
+    }
+  ];
+
+  const NavItem = ({item, index}) =>  {
+    const {name, link, image, alt} = item;
+    return (
+      <>
+        <div className="sidebar-link-container" onClick={(e) => changePage(e, index)}>
+          <Link href={link} className="sidebar-link" onClick={() => {
+              handleClick("Home")
+            }}>
+            <div className="sidebar-link-image">
+              <Image 
+                src="/Home Icon.svg"
+                width={44}
+                height={0}
+                alt="Home logo"
+              />
+            </div>
+            <div className={`sidebar-link-title 
+              ${activeIndex === index ? 'font-bold' : 'font-normal'}`
+            }>
+              Home
+            </div>
+          </Link>
+        </div>
+      </>
+    )
   }
 
   return (
     <nav className="sidebar">
       <div className="sidebar-container">
         <div className="sidebar-upper-section">
+          {navbarItems.map((value, index) => {
+            <NavItem item={value} index={index}/>
+          })}
           <div className="sidebar-link-container">
-            <Link href="/" className="sidebar-link" onClick={handleClick}>
+            <Link href="/" className="sidebar-link" onClick={() => {
+                handleClick("Home")
+              }}>
               <div className="sidebar-link-image">
                 <Image 
                   src="/Home Icon.svg"
