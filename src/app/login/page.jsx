8c81965e21ddc9cfe3c07873d729b844d6df7c8d
Alvis Yong <yong.mark.alvis@gmail.com>
2024-08-14 23:22:12 -0700

    Signups and inputs new user info in the database and authentication table

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,7 +1,7 @@
 "use client"; 
 import Link from "next/link"
 import { Inter } from "next/font/google"
-import { useState, useRef } from "react"
+import { useState } from "react"
 import "../../styles/accountpages.css"
 
 const inter = Inter( {
