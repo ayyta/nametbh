6c7030380168dfd8553760c4c089a42f7cda4d18
Alvis Yong <137463260+ayyta@users.noreply.github.com>
2024-09-04 15:30:28 -0700

    Addedd temporary logout button in profile page

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -1,9 +1,21 @@
+"use client";
+import { signOut } from "@/lib/auth";
+import { useRouter } from "next/navigation";
+
 export default function Profile() {
+  const router = useRouter();
+
+  const handleLogout = () => {
+    signOut(router);
+  }
   return (
     <main>
       <h1>
         Profile Page
       </h1>
+      <button className="w-24 h-12 bg-white" onClick={handleLogout}>
+        Logout
+      </button>
     </main>
   )
 }
