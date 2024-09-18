92cd3034bf17ab8ffe762a30419fc6f07ec7e602
Alvis Yong <137463260+ayyta@users.noreply.github.com>
2024-09-17 22:44:11 -0700

    Added header component to profile page

--- src/app/api/s3/route.js
+++ src/app/api/s3/route.js
@@ -1,7 +1,7 @@
-import { uploadFilesToS3, getFilesFromS3, getPresignedUrl } from "@/lib/s3Functions";
+import { uploadFilesToS3, getPresignedUrl, deleteFilesFromS3 } from "@/lib/s3Functions";
 import { NextResponse } from 'next/server';
 
-
+// Send file to S3 bucket
 export async function POST(req, res) {
   try {
     // Get file and path from request
@@ -33,23 +33,42 @@ export async function POST(req, res) {
   }
 }
 
+// Get image link (presigned URL) from S3 bucket
 export async function GET(req, res) {
   try {
     const url = new URL(req.url);
-    const images = url.searchParams.getAll("image");
+    const paths = url.searchParams.getAll("paths");
 
-    let imageList = [];
+    let pathsList = [];
 
     // Get files from S3
-    for (const s3Path of images) {
+    for (const s3Path of paths) {
       const url = await getPresignedUrl(s3Path);
       // Add image data to imageList if no error
-      imageList.push(url);
+      pathsList.push(url);
     }
 
-    return NextResponse.json({data: imageList}, { status: 200 });
+    return NextResponse.json({data: pathsList}, { status: 200 });
   } catch (error) {
-    console.error(error);
+    return NextResponse.json({error: error.message}, { status: 500 });
+  }
+}
+
+// Delete file from S3 bucket
+export async function DELETE(req, res) {
+  try {
+    const url = new URL(req.url);
+    const paths = url.searchParams.getAll("paths");
+
+    // Delete files from S3
+    for (const s3Path of paths) {
+      const response = await deleteFilesFromS3(s3Path);
+      if (response.error) {
+        return NextResponse.json({error: response.error}, { status: 500 });
+      }
+    }
+    return NextResponse.json({data: "success"}, { status: 200 });
+  } catch (error) {
     return NextResponse.json({error: error.message}, { status: 500 });
   }
 }
