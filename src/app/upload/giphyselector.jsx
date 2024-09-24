9ceed577943fa3c734eef71295e3d47a1e4c144f
Eric19La <Eric19La@github.com>
2024-09-24 11:27:20 -0700

    Need to make Search Bar reactive

--- src/app/upload/giphyselector.jsx
+++ src/app/upload/giphyselector.jsx
@@ -25,7 +25,7 @@ export default function GiphySelector({
       const results = await axios("https://api.giphy.com/v1/gifs/trending", {
         params: {
           api_key : '7VGHs9vybY2b6EcUZtVi3ay9pVVlFOxu',
-          limit : 24,
+          limit : 30,
         }
       });
       console.log(results);
@@ -61,7 +61,7 @@ export default function GiphySelector({
     <div className="flex fixed top-1/2 left-1/2 -translate-y-2/4 -translate-x-2/4 border-4 border-solid border-primary/50 rounded-2xl bg-third max-h-161 z-20">
       <div className="absolute -top-12 left-1/2 -translate-y-2/4 -translate-x-2/4">
         <form className="flex items-center justify-center w-max rounded-full bg-slate-200">
-          <div>
+          <div alt="Search Bar">
             <input 
               type="search" 
               placeholder="Search Gifs..." 
@@ -71,26 +71,33 @@ export default function GiphySelector({
           <div className="float-right ml-2 mr-2">
             <button 
               type="submit"
-              className="flex bg-transparent"
+              className="flex p-2 hover:rounded-full hover:bg-slate-300 active:opacity-50 active:bg-slate-400 cursor-pointer"
             >
-              GO
+              <Image
+                src="/Search Icon.svg"
+                width={28}
+                height={0}
+                alt='Search logo'
+              />
             </button>
           </div>
         </form>
       </div>
-      <div className="scrollbar grid grid-cols-3 relative overflow-y-hidden overflow-y-scroll scroll-smooth">
-        {showGifs && renderGifs()}
-        <button 
-          className='fixed -top-16 right-1.5 z-50 bg-slate-200 rounded-full hover:opacity-80 hover:bg-secondary active:opacity-50 cursor-pointer'
+      <div alt="Gif Close Button">
+        <button
+          className='fixed -top-16 right-3.5 z-50 bg-slate-200 rounded-full hover:bg-slate-300 active:opacity-50 cursor-pointer'
           onClick={closePopup}
         >
           <Image
             src="/Close Small Icon.svg"
-            width={42}
+            width={38}
             height={0}
             alt='Close logo'
           />
         </button>
+      </div>
+      <div className="gif-scrollbar grid grid-cols-3 relative overflow-y-hidden overflow-y-scroll scroll-smooth">
+        {showGifs && renderGifs()}
       </div>
     </div>
   );
