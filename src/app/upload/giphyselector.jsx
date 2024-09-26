5f8ae0ce5c4cfa6e08098ee4740bfde3bc262fca
Eric19La <eric.la7722@gmail.com>
2024-09-25 18:24:40 -0700

    Creating a loading screen when fetching the gifs

--- src/app/upload/giphyselector.jsx
+++ src/app/upload/giphyselector.jsx
@@ -1,3 +1,4 @@
+'use client';
 import { useEffect, useState } from "react";
 import Image from "next/image";
 import axios from "axios";
@@ -12,30 +13,74 @@ export default function GiphySelector({
 })
 
 {
+  const [searchGif, setSearchGif] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
-  if (!showPopup) return null;
-
+  // Closes the Giphy popup
   const closePopup = () => {
     setShowPopup(false);
     setShowGifs(false);
-  }
+  };
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const results = await axios("https://api.giphy.com/v1/gifs/trending", {
+  // Fetches the gifs from the Giphy API
+  const fetchData = async (searchVal = '') => {
+    setIsLoading(true);
+
+    try {
+      const endpoint = searchVal ? `https://api.giphy.com/v1/gifs/search` : 'https://api.giphy.com/v1/gifs/trending';
+
+      const results = await axios(endpoint, {
         params: {
-          api_key : '7VGHs9vybY2b6EcUZtVi3ay9pVVlFOxu',
+          api_key : `7VGHs9vybY2b6EcUZtVi3ay9pVVlFOxu`,
           limit : 30,
         }
-      });
-      console.log(results);
+      })
       setGifs(results.data.data);
+    } catch (error) {
+      console.error("Error fetching gifs", error);
     }
-    fetchData();
-  }, [showGifs]);
-  // }, []);
+  };
 
+  // Handles the user search change
+  const handleSearchChange = (e) => {
+    const searchVal = e.target.value;
+    setSearchGif(searchVal);
+    fetchData(searchVal);
+  };
+
+  // Whenever the user searches for a gif, it will fetch the data
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  // Handles the user search submission and gets the data they searched for
+  const handleSearchSubmit = async (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+
+    try {
+      const results = await axios("https://api.giphy.com/v1/gifs/search", {
+        params: {
+          api_key : '7VGHs9vybY2b6EcUZtVi3ay9pVVlFOxu',
+          q : searchGif,
+        }
+      });
+      setGifs(results.data.data);
+    } catch (error) {
+      console.error("Error fetching gifs", error);
+    }
+  };
+
+  // Renders the gifs
   const renderGifs = () => {
+    if (isLoading) {
+      return (
+        <div>
+          <p>Loading...</p>
+        </div>
+      );
+    }
+
     return gifs.map(giphy => {
       return (
         <div className="flex" key={giphy.id}>
@@ -54,15 +99,20 @@ export default function GiphySelector({
         </div>
       )
     });
-  }
+  };
 
+  if (!showPopup) return null;
 
   return (
     <div className="flex fixed top-1/2 left-1/2 -translate-y-2/4 -translate-x-2/4 border-4 border-solid border-primary/50 rounded-2xl bg-third max-h-161 z-20">
       <div className="absolute -top-12 left-1/2 -translate-y-2/4 -translate-x-2/4">
-        <form className="flex items-center justify-center w-max rounded-full bg-slate-200">
+        <form 
+          className="flex items-center justify-center w-max rounded-full bg-slate-200"
+        >
           <div alt="Search Bar">
             <input 
+              value={searchGif}
+              onChange={handleSearchChange}
               type="search" 
               placeholder="Search Gifs..." 
               className="flex bg-transparent text-lg text-primary p-3.5 focus:bg-slate-300 focus:rounded-full" 
@@ -72,6 +122,7 @@ export default function GiphySelector({
             <button 
               type="submit"
               className="flex p-2 hover:rounded-full hover:bg-slate-300 active:opacity-50 active:bg-slate-400 cursor-pointer"
+              onClick={handleSearchSubmit}
             >
               <Image
                 src="/Search Icon.svg"
