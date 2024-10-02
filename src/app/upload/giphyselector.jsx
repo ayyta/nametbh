981cf1954412d5d633b744960089f97097fd1f48
Eric19La <eric.la7722@gmail.com>
2024-10-02 01:15:38 -0700

    Finished the loading

--- src/app/upload/giphyselector.jsx
+++ src/app/upload/giphyselector.jsx
@@ -57,7 +57,7 @@ export default function GiphySelector({
     if (searchVal.length === 0) {
       setIsLoading(false);
     }
-  }, 600), []);
+  }, 100), []);
 
   // Handles the user search change
   const handleSearchChange = async (e) => {
@@ -151,27 +151,35 @@ export default function GiphySelector({
           </div>
         </form>
       </div>
-      <div alt="Gif Close Button">
-        <button
-          className='fixed -top-16 right-3.5 z-50 bg-slate-200 rounded-full hover:bg-slate-300 active:opacity-50 cursor-pointer'
-          onClick={closePopup}
-        >
-          <Image
-            src="/Close Small Icon.svg"
-            width={38}
-            height={0}
-            alt='Close logo'
-          />
-        </button>
-      </div>
       {isLoading && (
-        <div className="flex fixed top-1/2 left-1/2 -translate-y-2/4 -translate-x-2/4 border-solid border-primary/50 rounded-2xl bg-third z-30 w-96 h-14 bg-white">
-          <p>Loading...</p>
+        <div alt="Loading Container">
+          <button type="button" className="bg-primary text-white font-bold py-2 px-4 rounded border-4 border-slate-200 inline-flex items-center" disabled>
+            <svg className="animate-spin h-8 w-8 mr-3 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+              <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+              <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4z"></path>
+            </svg>
+            Loading GIF...
+          </button>
         </div>
       )}
-      <div className="gif-scrollbar grid grid-cols-3 relative overflow-y-hidden overflow-y-scroll scroll-smooth">
-        {showGifs && renderGifs()}
-      </div>
+      {(showGifs && !isLoading) && (
+        <div className="gif-scrollbar grid grid-cols-3 relative overflow-y-hidden overflow-y-scroll scroll-smooth">
+          {renderGifs()}
+          <div alt="Gif Close Button">
+            <button
+              className='fixed -top-16 right-3.5 z-50 bg-slate-200 rounded-full hover:bg-slate-300 active:opacity-50 cursor-pointer'
+              onClick={closePopup}
+            >
+              <Image
+                src="/Close Small Icon.svg"
+                width={38}
+                height={0}
+                alt='Close logo'
+              />
+            </button>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
